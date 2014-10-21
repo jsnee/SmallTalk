585bc582a69d32b300e052f022c6fe07d0787a39
Joseph Snee <snoey.92@gmail.com>
2014-10-21 10:06:09 -0500

    Allowed for start playing.

--- src/com/example/smalltalk/GameScreen.java
+++ src/com/example/smalltalk/GameScreen.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -12,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +25,8 @@ public class GameScreen extends Activity {
 	protected static int audioValue = 0;
 	protected static int maxAudioValue = 500;
 	protected static ProgressBar _progressBar;
-	protected static boolean isListening = false;
+	protected boolean isListening = false;
+	protected boolean isPlaying = false;
 	protected ArrayList<String> questions = new ArrayList<String>();
 	protected int currentQuestionIndex = 0;
 
@@ -47,13 +51,40 @@ public class GameScreen extends Activity {
 	public void advanceQuestion() {
 		if (questions.size() > currentQuestionIndex + 1) {
 			currentQuestionIndex++;
+			updateQuestionDisplay();
 		}
 	}
-	
+
 	public void updateQuestionDisplay() {
 		TextView textView = (TextView) findViewById(R.id.textView1);
 		char[] question = questions.get(currentQuestionIndex).toCharArray();
 		textView.setText(question, 0, question.length);
+	}
+
+	public void displayNextQuestionButton() {
+		ImageButton playNextButton = (ImageButton) findViewById(R.id.imageButtonPlayNext);
+		//playNextButton.setBackgroundResource(R.drawable.skip);
+		playNextButton.setImageResource(R.drawable.skip);
+		playNextButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View view) {
+				advanceQuestion();
+			}
+
+		});
+	}
+
+	public void playOnClick(View view) {
+		isPlaying = true;
+		startPlaying();
+		displayNextQuestionButton();
+	}
+
+	public void startPlaying() {
+		shuffleQuestions();
+		currentQuestionIndex = 0;
+		updateQuestionDisplay();
 	}
 
 	public void loadQuestions(String categoryName) {
@@ -132,24 +163,24 @@ public class GameScreen extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main, menu);
-	    return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item 
-	     switch(item.getItemId())
-	        {
-	            case  R.id.settings:
-	            	//Settings selected
-	            	startActivity(new Intent(this, SettingScreen.class));
-	                return true;	                
-	            default:
-	                return super.onOptionsItemSelected(item);
+		switch(item.getItemId())
+		{
+		case  R.id.settings:
+			//Settings selected
+			startActivity(new Intent(this, SettingScreen.class));
+			return true;	                
+		default:
+			return super.onOptionsItemSelected(item);
 
-	        }
-	     
+		}
+
 	}
 }
